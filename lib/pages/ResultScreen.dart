31f742d3d527c18ac67b3b86cf1b78c4237d0d0a
Zahin <Zaheen.kenji@gmail.com>
2023-09-22 19:13:25 +0600

    added pagelogo, added result text

--- lib/pages/ResultScreen.dart
+++ lib/pages/ResultScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mind_reader/Global.dart';
 import 'package:mind_reader/pages/HomePage.dart';
 
 class ResultScreen extends StatefulWidget {
@@ -11,17 +12,52 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: AppBar(
-    backgroundColor: Colors.redAccent,
-    leading: const Icon(Icons.adb), //later change to custom icon
-    title: const Text("Mind Reader"),),
+        backgroundColor: Colors.redAccent,
+        leading: const Icon(Icons.adb), //later change to custom icon
+        title: const Text("Mind Reader"),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ElevatedButton(onPressed: (){
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
-          }, child: Text("Back to home"))
+          Image.asset("assets/images/resultpage-logo.png"),
+          SizedBox(height: 30,),
+          Center(
+            child: const Text(
+              "Your thoughts were:",
+              style: TextStyle(fontSize: 27),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Card(
+            color: Colors.white70,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                thoughts!,
+                style: TextStyle(fontSize: 35),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("Try Again",style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              ))
         ],
       ),
     ));

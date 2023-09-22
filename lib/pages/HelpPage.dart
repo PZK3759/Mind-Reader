617668628e2159286848826a3b254b242ec2d82e
Zahin <Zaheen.kenji@gmail.com>
2023-09-22 17:12:43 +0600

    added HelpPage

---
+++ lib/pages/HelpPage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HelpPage extends StatelessWidget {
+  const HelpPage({super.key});
+
+  //String helpText = "This innovative app employs advanced neurotechnology to interpret your thoughts. Simply input your thoughts into the textbox and press the button. The app seamlessly intercepts your brain's neurotransmitter signals, providing real-time insight into your thoughts";
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text("How does this work"),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: const Center(
+        child: Card(
+          color: Colors.red,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "This innovative app employs advanced neurotechnology to interpret your thoughts. Simply input your thoughts into the textbox and press the button. The app seamlessly intercepts your brain's neurotransmitter signals, providing real-time insight into your thoughts",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ),
+      ),
+    ));
+  }
+}
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mind_reader/pages/HelpPage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +11,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      appBar: AppBar(title: const Text("Mind Reader"),),
-    ));
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.redAccent,
+            leading: const Icon(Icons.adb), //later change to custom icon
+            title: const Text("Mind Reader"),
+            actions: [
+              IconButton(onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HelpPage()));
+              }, icon: const Icon(Icons.question_mark))
+            ],
+
+          ),
+        ));
   }
 }
